ec9b6911267cdf364691a689638d5d430e2e856b
Patrick Hunt <phunt1@gmail.com>
2011-12-13 11:28:02 -0800

    Updated now that reviews.apache.org has the proper repository path for the git repo

--- zpost-review.sh
+++ zpost-review.sh
@@ -9,4 +9,4 @@ REVLIST=$1;
 JIRA=$2
 shift 2;
 
-~/dev/myrbtools/rbtools/postreview.py --repository-url="/home/reviewboard/zookeeper/.git" --server="https://reviews.apache.org" --target-groups=zookeeper --summary="$(git log --pretty=format:%s $REVLIST)" --description="$(git whatchanged $REVLIST)" --diff-filename=<(git diff --no-prefix $REVLIST) -o --bugs-closed=ZOOKEEPER-$JIRA $*
+post-review --server="https://reviews.apache.org" --target-groups=zookeeper --summary="$(git log --pretty=format:%s $REVLIST)" --description="$(git whatchanged $REVLIST)" --diff-filename=<(git diff --no-prefix $REVLIST) -o --bugs-closed=ZOOKEEPER-$JIRA $*
